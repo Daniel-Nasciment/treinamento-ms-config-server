f74053ba392d4d8b0821b9e6ce5542bcddde7fb1
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-22 13:12:11 -0300

    Habilitando config server

--- src/main/java/br/com/alura/microservice/eureka/ConfigServerApplication.java
+++ src/main/java/br/com/alura/microservice/eureka/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.microservice.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // JA ESTA HABILITADO O SERVIDOR DE CONFIGURACAO
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
